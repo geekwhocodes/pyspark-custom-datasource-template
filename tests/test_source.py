b795f909bd8428d5d9aa2a7f6586fe624cc9a989
geekwhocodes <ganeshraskar@outlook.com>
2025-02-21 20:23:37 +0000

    conftest

--- tests/test_source.py
+++ tests/test_source.py
@@ -3,10 +3,10 @@ from pyspark.sql import SparkSession
 from source_faker.source import FakeDataSource
 
 
-@pytest.fixture
-def spark():
-    spark = SparkSession.builder.getOrCreate()
-    yield spark
+# @pytest.fixture
+# def spark():
+#     spark = SparkSession.builder.getOrCreate()
+#     yield spark
 
 
 def test_fake_datasource(spark):
